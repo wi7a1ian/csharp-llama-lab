dc3f27086c46ffcef974543d796c727522505bd5
Wi7a1ian <witalian+github@pm.me>
2023-06-28 14:11:30 +0200

    Initial checkin

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # csharp-llama-lab
+1. Download model [wizardLM-7B.ggmlv3.q4_1.bin](https://huggingface.co/TheBloke/wizardLM-7B-GGML/resolve/main/wizardLM-7B.ggmlv3.q4_1.bin) from [Hugging Face](https://huggingface.co/TheBloke/wizardLM-7B-GGML)
+2. Run
+3. ...
+4. Profit
---
+++ src/Program.cs
@@ -0,0 +1,26 @@
+﻿using LLama.Common;
+using LLama;
+
+var model = new LLamaModel(new ModelParams(
+    modelPath: Path.Combine("C:", "Models", "wizardLM-7B.ggmlv3.q4_1.bin"),
+    contextSize: 1024, 
+    seed: 1337, 
+    gpuLayerCount: 5));
+
+var ex = new InteractiveExecutor(model);
+var session = new ChatSession(ex);
+
+var prompt = "Transcript of a dialogdotnet run, where the User interacts with an Assistant named Bob. Bob is helpful, kind, honest, good at writing, and never fails to answer the User's requests immediately and with precision.\r\n\r\nUser: Hello, Bob.\r\nBob: Hello. How may I help you today?\r\nUser: Please tell me the largest city in Europe.\r\nBob: Sure. The largest city in Europe is Moscow, the capital of Russia.\r\nUser:";
+Console.WriteLine();
+Console.Write(prompt);
+while (true)
+{
+    foreach (var text in session.Chat(prompt, new InferenceParams() { Temperature = 0.6f, AntiPrompts = new List<string> { "User:" } }))
+    {
+        Console.Write(text);
+    }
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    prompt = Console.ReadLine();
+    Console.ForegroundColor = ConsoleColor.White;
+}
